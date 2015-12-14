997f783dc65085372a171e532ffcccb0ee251624
Wusung Peng <wusungpeng@kkbox.com>
2015-12-14 10:18:20 +0800

    Initial commit

---
+++ src/main/java/sample/batch/SampleBatchApplication.java
@@ -0,0 +1,65 @@
+package sample.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author wusungpeng
+ * @since 12/14/15
+ */
+@SpringBootApplication
+@EnableBatchProcessing
+public class SampleBatchApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleBatchApplication.class.getName());
+
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Bean
+    protected Tasklet tasklet() {
+        logger.info("Z");
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution,
+                                        ChunkContext context) {
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    @Bean
+    public Job job() throws Exception {
+        return this.jobs.get("job").start(step1()).build();
+    }
+
+    @Bean
+    protected Step step1() throws Exception {
+        return this.steps.get("step1").tasklet(tasklet()).build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // System.exit is common for Batch applications since the exit code can be used to
+        // drive a workflow
+        System.exit(SpringApplication
+                .exit(SpringApplication.run(SampleBatchApplication.class, args)));
+
+    }
+}
